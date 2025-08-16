64078320089ff0b2bf6df2bf78c1c156d33c2069
Irina PV <perezeginaiv@mail.ru>
2025-08-16 18:36:13 +0500

    добавлен компонент текстового поля

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,25 +1,32 @@
 import { memo } from "react";
+import { ReactComponent as Search } from "../assets/loupe.svg";
+import { Input } from "../components/Input/Input";
 import { PageLayout } from "../components/PageLayout/PageLayout";
-import { Select, type SelectOption } from "../components/Select/Select";
 
-const optionsGender: SelectOption[] = [
-  { id: "gender-1", content: "Female" },
-  { id: "gender-2", content: "Male" },
-  { id: "gender-3", content: "Genderless" },
-  { id: "gender-4", content: "Unknown" },
-];
+// const optionsGender: SelectOption[] = [
+//   { id: "gender-1", content: "Female" },
+//   { id: "gender-2", content: "Male" },
+//   { id: "gender-3", content: "Genderless" },
+//   { id: "gender-4", content: "Unknown" },
+// ];
 
-const optionsStatus: SelectOption[] = [
-  { id: "status-1", content: "Alive", status: "green" },
-  { id: "status-2", content: "Dead", status: "red" },
-  { id: "status-3", content: "Unknown", status: "orange" },
-];
+// const optionsStatus: SelectOption[] = [
+//   { id: "status-1", content: "Alive", status: "green" },
+//   { id: "status-2", content: "Dead", status: "red" },
+//   { id: "status-3", content: "Unknown", status: "orange" },
+// ];
 
 export const MainPage = memo(() => {
   return (
     <PageLayout>
-      <Select view="big" options={optionsGender} />
-      <Select view="small" options={optionsStatus} />
+      <Input
+        Svg={<Search />}
+        view="filter"
+        value=""
+        placeholder="Filter by name..."
+        onChange={() => {}}
+      />
+      <Input view="form" value="Rick Sanchez" onChange={() => {}} />
     </PageLayout>
   );
 });
